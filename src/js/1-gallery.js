30e5cd91424ba3be5b9976dc49a4928fa0bcf4d0
Marina.S. <marina.sevostian@gmail.com>
2024-10-15 13:00:58 +0300

    modified link

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -83,6 +83,7 @@ gallery.insertAdjacentHTML('beforeend', galleryUp);
 
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+
 new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionDelay: 250,
