dda14adbb3ede0811d9499d4dae81c1a6d7efb99
Marina.S. <marina.sevostian@gmail.com>
2024-10-17 04:02:49 +0300

    add form js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,45 @@
-// let formData{ email: "", message: "" };
+let formData = { email: '', message: '' };
+
+const formEl = document.querySelector('.feedback-form');
+const localStorageKey = 'feedback-form-state';
+
+loadForm();
+
+formEl.addEventListener('input', onSaveFormInput);
+
+formEl.addEventListener('submit', onFormSubmit);
+
+function onSaveFormInput(event) {
+  formData = JSON.parse(localStorage.getItem(localStorageKey)) || {};
+
+  formData[event.target.name] = event.target.value;
+
+  localStorage.setItem(localStorageKey, JSON.stringify(formData));
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  if (!event.target.email.value || !event.target.message.value) {
+    alert('Fill please all fields');
+    return;
+  }
+
+  event.target.reset();
+  console.log(formData);
+  localStorage.removeItem(localStorageKey);
+}
+
+function loadForm() {
+  try {
+    let formLoad = JSON.parse(localStorage.getItem(localStorageKey));
+    if (!formLoad) {
+      return;
+    }
+
+    formData = formLoad;
+    formEl.email.value = formData.email || '';
+    formEl.message.value = formData.message || '';
+  } catch (error) {
+    console.error('Error.message ', error.message);
+  }
+}
